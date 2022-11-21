a3321d93ff14737d25474cd2f86e53f11fe07d7f
Helder Fonseca <helder2391@gmail.com>
2022-11-21 10:16:29 -0100

    fix conflit

--- components/PacoteList.js
+++ components/PacoteList.js
@@ -25,13 +25,8 @@ export default function PacoteList() {
     return(
         <>
         <section className="mt-32 flex flex-col justify-center items-center mx-auto md:w-1/2 w-full">
-<<<<<<< HEAD
-            <h1 className="px-5 md:px-0 text-5xl font-bold md:inline-block rounded-md text-[#070707] text-center">Aposte cada vez mais no digital </h1>
+            <h1 className="px-7 md:px-0 text-5xl font-bold md:inline-block rounded-md text-[#070707] text-center">Aposte cada vez mais no digital </h1>
                 <h2 className="mt-5 mb-4 px-10 md:px-0 text-center md:w-[27rem] text-3xl font-light italic">
-=======
-            <h1 className="px-7 md:px-0 text-5xl font-bold md:inline-block p-2 rounded-md text-[#070707] text-center">Aposte cada vez mais no digital </h1>
-                <h2 className="mt-5 mb-4 px-16 md:px-0 text-center md:w-[27rem] text-3xl font-light italic">
->>>>>>> c89eaf75c4ff4381b44c4c64b031392f68f62607
                     <span className="font-bold">Divulgue seu negócio no print e no digital! </span>
                         Com o pacote Yellow Connect</h2>
                 <div className="pt-5">
